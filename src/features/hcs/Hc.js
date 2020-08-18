389d3783e73cad762b4a3f2b936dcdd99e642cfd
replacem3nts <donny.landis@gmail.com>
2020-08-18 16:57:16 -0400

    fixes rx component to be used for both loaded and picked up

--- src/features/hcs/Hc.js
+++ src/features/hcs/Hc.js
@@ -1,41 +1,13 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
+import { PickedUpPrescription } from '../prescriptions/PickedUpPrescription'
 
 const Hc = ({ hc }) => {
-    const [toLoad, setToLoad] = useState([])
-
     const pickedup = hc.rxes.filter(rx => rx.pickedup && !rx.loaded)
     const loaded = hc.rxes.filter(rx => rx.loaded)
 
-    let dateOptions = {
-        month: 'short',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-    }
-
-    let handleToLoad = (rxId) => {
-        if(toLoad.find(tl => tl === rxId)){
-            setToLoad(toLoad => toLoad.filter(id => id !== rxId))
-        } else {
-            setToLoad(toLoad => [...toLoad, rxId])
-        }
-    }
-
-
     const pickedupRxArr = pickedup.map(rx => {
-        return (
-            <li>
-                <span>{rx.rxcode}</span>
-                <span>{new Intl.DateTimeFormat('en-US', dateOptions).format(new Date(rx.appt))}</span>
-                <span>{rx.cardserial}</span>
-                <span>{rx.pickedup ? 'Yes' : 'No'}</span>
-                <span>{rx.loaded ? 'Yes' : 'No'}</span>
-                <span>
-                    <input type='checkbox' checked={toLoad.find(rxId => rxId === rx.id)} value={rx.id} onClick={(e) => handleToLoad(e.target.value)}/>
-                </span>
-            </li>
-            )
+        return <PickedUpPrescription key={rx.id} {...rx}/>
         })
 
     return (
@@ -43,21 +15,22 @@ const Hc = ({ hc }) => {
             <section>
                 <h2>{hc.name}</h2>
             </section>
-            <section>
+            <section className='display-container'>
                 <h3>Picked Up:</h3>
-                <form>
-                    <div>
-                        <span>Rx Code</span>
-                        <span>Appt.</span>
-                        <span>Card Serial</span>
-                        <span>Picked Up</span>
-                        <span>Loaded</span>
-                        <span>Load?</span>
-                        <ul>
-                            {pickedupRxArr}
-                        </ul>
-                    </div>
-                </form>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Rx Code</th>
+                            <th>Appt.</th>
+                            <th>Card Serial</th>
+                            <th>Picked Up</th>
+                            <th>Loaded</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {pickedupRxArr}
+                    </tbody>
+                </table>
             </section>
             <section>
             <h3>Loaded:</h3>
--- src/features/prescriptions/PickedUpPrescription.js
+++ src/features/prescriptions/PickedUpPrescription.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const PickedUpPrescription = ({ id, rxcode, appt, cardserial, pickedup, loaded, edit}) => {
+export const PickedUpPrescription = ({ id, rxcode, appt, cardserial, pickedup, loaded }) => {
+    const [loadStatus, setLoadStatus] = useState(loaded)
+
+    let dateOptions = {
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    }
+
+    let handleChange = () => {
+
+    }
 
     return (
-        <tr onClick={() => edit(id)}>
+        <tr>
             <td>{rxcode}</td>
-            <td>{appt}</td>
+            <td>{new Intl.DateTimeFormat('en-US', dateOptions).format(new Date(appt))}</td>
             <td>{cardserial}</td>
             <td>{pickedup ? 'Yes' : 'No'}</td>
-            <td>{loaded ? 'Yes' : 'No'}</td>
-            <td><input/> </td>
+            <td>
+                <form onChange={handleChange}>
+                    <select value={loadStatus} onChange={(e) => setLoadStatus(e.target.value)} >
+                        <option value={true} >Yes</option>
+                        <option value={false} >No</option>
+                    </select>
+                </form>
+            </td>
         </tr>
     )
 }
